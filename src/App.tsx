2313239ebbed771e81ade31bf6f14448da500b69
xioperez01 <angie.xioperez@gmail.com>
2022-06-19 13:35:11 -0500

    Testing Actions

--- src/App.tsx
+++ src/App.tsx
@@ -21,15 +21,7 @@ export const App = (): JSX.Element => (
           <Text>
             Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
           </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+          <Text>Testin actions</Text>
         </VStack>
       </Grid>
     </Box>
