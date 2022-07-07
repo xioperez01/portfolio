03c19ed469768bf916fadbbcb4280e9cefcbd6fd
xioperez01 <angie.xioperez@gmail.com>
2022-07-07 11:33:18 -0500

    fix React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Body } from "./Body";
 import { AngieTheme } from "./CustomTheme";
 import i18n from "./locale/i18n";
@@ -8,13 +8,13 @@ i18n.init();
 
 export const App = (): JSX.Element => {
   return (
-    <Router>
+    <HashRouter>
       <ChakraProvider theme={AngieTheme}>
         <ColorModeProvider>
           <Body />
         </ColorModeProvider>
       </ChakraProvider>
-    </Router>
+    </HashRouter>
   );
 };
 
